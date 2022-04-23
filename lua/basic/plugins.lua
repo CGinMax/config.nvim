900f63bd0a11184ab6e4917ee7e836c3ead1ce62
CGinMax <cginmax1998@gmail.com>
2022-04-23 14:19:00 +0800

    add some plugin and jetbrains nerd icon font

--- lua/basic/plugins.lua
+++ lua/basic/plugins.lua
@@ -11,8 +11,43 @@ packer.startup(
       -- lsp
 			use {
 			  "neovim/nvim-lspconfig",
-			  "williamboman/nvim-lsp-installer"
+        config = function()
+          require("plugin-config.nvim-lspconfig")
+        end
 			}
+      use {
+			  "williamboman/nvim-lsp-installer",
+        config = function()
+          require("plugin-config.nvim-lsp-installer")
+        end
+      }
+      use {
+        "tami5/lspsaga.nvim",
+        config = function()
+          require("plugin-config.lspsaga")
+        end
+      }
+      -- lsp_signature:插入模式传参提示
+      use {
+        "ray-x/lsp_signature.nvim",
+        config = function()
+            require("plugin-config.lsp_signature")
+        end
+      }
+      -- fidget:lsp进度提示
+      use {
+        "j-hui/fidget.nvim",
+        config = function()
+          require("plugin-config.fidget")
+        end
+      }
+      -- nvim-lightbulb:灯泡提示
+      use {
+        "kosayoda/nvim-lightbulb",
+        config = function()
+            require("plugin-config.nvim-lightbulb")
+        end
+      }
       -- nvim-tree
       use {
         "kyazdani42/nvim-tree.lua",
@@ -41,21 +76,43 @@ packer.startup(
         end
       }
       -- comment.nvim
+      -- use {
+      --   "numToStr/Comment.nvim",
+      --   config = function()
+      --     -- require("plugin-config.comment-nvim")
+      --     require("Comment").setup()
+      --   end
+      -- }
+
+      -- catppuccin.nvim:界面美化
       use {
-        "numToStr/Comment.nvim",
+        "catppuccin/nvim",
+        -- 仓库命为nvim，有冲突，进行as做别名
+        as = "catppuccin",
         config = function()
-          require("plugin-config.comment-nvim")
+          require("plugin-config.catppuccin")
+        end
+      }
+
+      -- 支持 LSP 状态的 buffer 栏
+      use {
+        "akinsho/bufferline.nvim",
+        requires = {
+            "famiu/bufdelete.nvim" -- 删除 buffer 时不影响现有布局
+        },
+        config = function()
+            require("plugin-config.bufferline")
         end
       }
 
       -- telescope.nvm:模糊搜索
-      use {
-        "nvim-telescope/telescope.nvim",
-        requires = {{"nvim-lua/plenary.nvim"}},
-        config = function()
-          require("plugin-config.telescope-nvim")
-        end
-      }
+      -- use {
+      --   "nvim-telescope/telescope.nvim",
+      --   requires = {{"nvim-lua/plenary.nvim"}},
+      --   config = function()
+      --     require("plugin-config.telescope-nvim")
+      --   end
+      -- }
 
       -- windline
       --use {
