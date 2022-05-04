8f4cc49b02cdc0bf6c3e4d0cd380b910bdc73aa8
CGinMax <cginmax1998@gmail.com>
2022-05-04 23:07:24 +0800

    update volar ts

--- lua/lsp/eslint.lua
+++ lua/lsp/eslint.lua
@@ -3,7 +3,7 @@ local home_dir = os.getenv("HOME")
 local lua_lsp_bin = string.format("%s/%s", home_dir, ".local/share/nvim/lsp_servers/vscode-eslint/node_modules/vscode-langservers-extracted/bin/vscode-eslint-language-server")
 return {
   cmd = {lua_lsp_bin},
-  filetypes = {"ts", "tsx", "js", "jsx"},
+  filetypes = {"ts", "tsx", "js", "jsx", "vue"},
   log_level = 2,
   root_dir = function()
     return vim.fn.getcwd()
--- lua/lsp/volar.lua
+++ lua/lsp/volar.lua
@@ -1,12 +1,51 @@
 -- volar
 local home_dir = os.getenv("HOME")
 local lua_lsp_bin = string.format("%s/%s", home_dir, ".local/share/nvim/lsp_servers/volar/node_modules/@volar/vue-language-server/bin/vue-language-server.js")
+local tsserver_path = string.format("%s/%s", home_dir, ".local/share/nvim/lsp_servers/tsserver/node_modules/typescript/bin/tsserver")
 return {
-  cmd = {lua_lsp_bin},
-  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+  cmd = {lua_lsp_bin, "--stdio"},
+  filetypes = {"vue"},
   log_level = 2,
   root_dir = function()
     return vim.fn.getcwd()
   end,
   single_file_support = true,
+  init_options = {
+    documentFeatures = {
+      documentColor = false,
+      documentFormatting = {
+        defaultPrintWidth = 100
+      },
+      documentSymbol = true,
+      foldingRange = true,
+      linkedEditingRange = true,
+      selectionRange = true
+    },
+    languageFeatures = {
+      callHierarchy = true,
+      codeAction = true,
+      codeLens = true,
+      completion = {
+        defaultAttrNameCase = "kebabCase",
+        defaultTagNameCase = "both"
+      },
+      definition = true,
+      diagnostics = true,
+      documentHighlight = true,
+      documentLink = true,
+      hover = true,
+      implementation = true,
+      references = true,
+      rename = true,
+      renameFileRefactoring = true,
+      schemaRequestService = true,
+      semanticTokens = false,
+      signatureHelp = true,
+      typeDefinition = true
+    },
+    typescript = {
+      serverPath = tsserver_path
+    }
+  }
+  ,
 }
