f8ec280907a34fc5cb15dd46917bfc5ddfe4bbb1
CGinMax <cginmax@CooperdeMacBook-Pro.local>
2023-03-23 12:31:58 +0800

    refactor:change packer manager to lazy

--- lua/basic/keybinds.lua
+++ lua/basic/keybinds.lua
@@ -21,6 +21,67 @@ vim.keybinds.gmap("n", "<C-j>", "<C-w>j", vim.keybinds.opts)
 vim.keybinds.gmap("n", "<C-k>", "<C-w>k", vim.keybinds.opts)
 vim.keybinds.gmap("n", "<C-l>", "<C-w>l", vim.keybinds.opts)
 
+-- nvim-tree
+-- 按 leader 1 打开文件树
+vim.keybinds.gmap("n", "<leader>1", "<cmd>NvimTreeToggle<CR>", vim.keybinds.opts)
+-- 按 leader fc 在文件树中找到当前以打开文件的位置
+vim.keybinds.gmap("n", "<leader>fc", "<cmd>NvimTreeFindFile<CR>", vim.keybinds.opts)
+
+-- 默认按键
+-- o     ：打开目录或文件
+-- a     ：新增目录或文件
+-- r     ：重命名目录或文件
+-- x     ：剪切目录或文件
+-- c     ：复制目录或文件
+-- d     ：删除目录或文件
+-- y     ：复制目录或文件名称
+-- Y     ：复制目录或文件相对路径
+-- gy    ：复制目录或文件绝对路径
+-- p     ：粘贴目录或文件
+-- s     ：使用系统默认程序打开目录或文件
+-- <Tab> ：将文件添加到缓冲区，但不移动光标
+-- <C-v> ：垂直分屏打开文件
+-- <C-x> ：水平分屏打开文件
+-- <C-]> ：进入光标下的目录
+-- <C-r> ：重命名目录或文件，删除已有目录名称
+-- -     ：返回上层目录
+-- I     ：切换隐藏文件/目录的可见性
+-- H     ：切换点文件的可见性
+-- R     ：刷新资源管理器
+-- 另外，文件资源管理器操作和操作文档方式一致，可按 / ? 进行搜索
+
+-- BufferLine
+-- 关闭当前 buffer，由 bufdelete 插件所提供的方法
+vim.keybinds.gmap("n", "<C-q>", "<cmd>Bdelete!<CR>", vim.keybinds.opts)
+-- 切换上一个缓冲区
+vim.keybinds.gmap("n", "t]", ":BufferLineCycleNext<CR>", vim.keybinds.opts)
+-- 切换下一个缓冲区
+vim.keybinds.gmap("n", "t[", ":BufferLineCyclePrev<CR>", vim.keybinds.opts)
+-- 关闭左侧缓冲区
+vim.keybinds.gmap("n", "<leader>bh", "<cmd>BufferLineCloseLeft<CR>", vim.keybinds.opts)
+-- 关闭右侧缓冲区
+vim.keybinds.gmap("n", "<leader>bl", "<cmd>BufferLineCloseRight<CR>", vim.keybinds.opts)
+
+vim.keybinds.gmap("n", "t1", ":BufferLineGoToBuffer 1<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "t2", ":BufferLineGoToBuffer 2<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "t3", ":BufferLineGoToBuffer 3<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "t4", ":BufferLineGoToBuffer 4<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "t5", ":BufferLineGoToBuffer 5<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "t6", ":BufferLineGoToBuffer 6<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "t7", ":BufferLineGoToBuffer 7<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "t8", ":BufferLineGoToBuffer 8<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "t9", ":BufferLineGoToBuffer 9<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "t$", ":BufferLineGoToBuffer -1<CR>", vim.keybinds.opts)
+
+-- notify
+-- 使用案例：
+-- 信息、级别、标题
+-- 级别有：info、warn、error、debug、trace
+-- 示例：
+-- vim.notify("hello world", "info", {title = "info"})
+-- 显示历史弹窗记录
+vim.keybinds.gmap( "n", "<leader>fn", "<cmd>lua require('telescope').extensions.notify.notify()<CR>", vim.keybinds.opts)
+
 -- winshift
 vim.keybinds.gmap("n", "<C-W>m", ":WinShift<CR>", vim.keybinds.opts)
 
@@ -33,6 +94,22 @@ vim.keybinds.gmap("n", "<C-Right>", ":vertical resize +2<CR>", vim.keybinds.opts
 -- Move tetx up and down
 vim.keybinds.gmap("n", "<A-j>", "<Esc>:m .+1<CR>==gi<Esc>", vim.keybinds.opts)
 vim.keybinds.gmap("n", "<A-k>", "<Esc>:m .-2<CR>==gi<Esc>", vim.keybinds.opts)
+
+-- Telescope
+-- 查找文件
+vim.keybinds.gmap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", vim.keybinds.opts)
+-- 查找文字
+vim.keybinds.gmap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", vim.keybinds.opts)
+-- 查找特殊符号
+vim.keybinds.gmap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", vim.keybinds.opts)
+-- 查找帮助文档
+vim.keybinds.gmap("n", "<leader>fh", "<cmd>Telescope help_tags <CR>", vim.keybinds.opts)
+-- 查找最近打开的文件
+vim.keybinds.gmap("n", "<leader>fo", "<cmd>Telescope oldfiles <CR>", vim.keybinds.opts)
+-- 查找 marks 标记
+vim.keybinds.gmap("n", "<leader>fm", "<cmd>Telescope marks <CR>", vim.keybinds.opts)
+-- 查看工程
+vim.keybinds.gmap("n", "<leader>fp", "<cmd>Telescope projects <CR>", vim.keybinds.opts)
 
 -- Trouble
 vim.keybinds.gmap("n", "<leader>tt", "<cmd>TroubleToggle workspace_diagnostics<CR>", vim.keybinds.opts)
--- lua/basic/plugins.lua
+++ lua/basic/plugins.lua
@@ -1,165 +1,119 @@
 ---@diagnostic disable: undefined-global
--- https://github.com/wbthomason/packer.nvim
+-- https://github.com/folke/lazy.nvim
 
-local fn = vim.fn
-local install_path = fn.stdpath("data").. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print ("Installing packer close and reopen NeoVim...")
-  vim.cmd [[packadd packer.nvim]]
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+vim.opt.rtp:prepend(lazypath)
+-- vim.cmd [[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]]
 
-local status_ok, packer = pcall(require, "packer")
+local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
-  print("wbthomason/packer.nvim load failed!")
+  print("folke/lazy.nvim load failed!")
   return
 end
 
-packer.init({
-    display = {
-        open_fn = function()
-            return require("packer.util").float({ border = "rounded" })
-        end,
-    },
-    profile = {
-      enable = true,
-      threshold = 1
-    }
-})
+-- packer.init({
+--     display = {
+--         open_fn = function()
+--             return require("packer.util").float({ border = "rounded" })
+--         end,
+--     },
+--     profile = {
+--       enable = true,
+--       threshold = 1
+--     }
+-- })
 
---  useage
--- use {
---   "myusername/example",        -- The plugin location string
---   -- The following keys are all optional
---   disable = boolean,           -- Mark a plugin as inactive
---   as = string,                 -- Specifies an alias under which to install the plugin
---   installer = function,        -- Specifies custom installer. See "custom installers" below.
---   updater = function,          -- Specifies custom updater. See "custom installers" below.
---   after = string or list,      -- Specifies plugins to load before this plugin. See "sequencing" below
---   rtp = string,                -- Specifies a subdirectory of the plugin to add to runtimepath.
---   opt = boolean,               -- Manually marks a plugin as optional.
---   branch = string,             -- Specifies a git branch to use
---   tag = string,                -- Specifies a git tag to use. Supports "*" for "latest tag"
---   commit = string,             -- Specifies a git commit to use
---   lock = boolean,              -- Skip updating this plugin in updates/syncs. Still cleans.
---   run = string, function, or table, -- Post-update/install hook. See "update/install hooks".
---   requires = string or list,   -- Specifies plugin dependencies. See "dependencies".
---   rocks = string or list,      -- Specifies Luarocks dependencies for the plugin
---   config = string or function, -- Specifies code to run after this plugin is loaded.
---   -- The setup key implies opt = true
---   setup = string or function,  -- Specifies code to run before this plugin is loaded.
---   -- The following keys all imply lazy-loading and imply opt = true
---   cmd = string or list,        -- Specifies commands which load this plugin. Can be an autocmd pattern.
---   ft = string or list,         -- Specifies filetypes which load this plugin.
---   keys = string or list,       -- Specifies maps which load this plugin. See "Keybindings".
---   event = string or list,      -- Specifies autocommand events which load this plugin.
---   fn = string or list          -- Specifies functions which load this plugin.
---   cond = string, function, or list of strings/functions,   -- Specifies a conditional test to load this plugin
---   module = string or list      -- Specifies Lua module names for require. When requiring a string which starts
---                                -- with one of these module names, the plugin will be loaded.
---   module_pattern = string/list -- Specifies Lua pattern of Lua module names for require. When
---   requiring a string which matches one of these patterns, the plugin will be loaded.
--- }
-
-return packer.startup(function (use)
-  use "wbthomason/packer.nvim"
-
+return lazy.setup({
   -- lsp
-  use "neovim/nvim-lspconfig"
+  "neovim/nvim-lspconfig",
   -- 通知
-  use "rcarriga/nvim-notify"
+  "rcarriga/nvim-notify",
 
-  use {
+  {
     "williamboman/mason.nvim",
-    requires = {
+    dependencies = {
       {"hrsh7th/cmp-nvim-lsp"}, -- 替换内置 omnifunc，获得更多补全
     },
-  }
-  use "williamboman/mason-lspconfig"
+  },
+  "williamboman/mason-lspconfig",
   --[[
-  use "williamboman/nvim-lsp-installer"
+  "williamboman/nvim-lsp-installer",
   ]]--
-  use "tami5/lspsaga.nvim"
+  "simrat39/rust-tools.nvim",
+  "tami5/lspsaga.nvim",
   -- lsp_signature:插入模式传参提示
-  use "ray-x/lsp_signature.nvim"
+  "ray-x/lsp_signature.nvim",
   -- fidget:lsp进度提示
-  use "j-hui/fidget.nvim"
+  "j-hui/fidget.nvim",
+
+  "lewis6991/hover.nvim",
 
   -- nvim-lightbulb:灯泡提示
-  use {
+  {
     "kosayoda/nvim-lightbulb",
-    requires = 'antoinemadec/FixCursorHold.nvim'
-  }
+    dependencies = "antoinemadec/FixCursorHold.nvim",
+  },
   -- null-ls:前端代码格式
-  use "jose-elias-alvarez/null-ls.nvim"
+  "jose-elias-alvarez/null-ls.nvim",
   -- prettier:前端代码美化
-  use {
+  {
     "MunifTanjim/prettier.nvim",
-    requires = {
+    dependencies = {
       "neovim/nvim-lspconfig",
       "jose-elias-alvarez/null-ls.nvim"
     },
-  }
+  },
   -- dap
-  use "ravenxrz/DAPInstall.nvim" -- help us install several debuggers
+  "ravenxrz/DAPInstall.nvim", -- help us install several debuggers
   -- nvim-dap
-  use "mfussenegger/nvim-dap"
-  use "theHamsta/nvim-dap-virtual-text"
-  use "rcarriga/nvim-dap-ui"
-  use { "jbyuki/one-small-step-for-vimkind", module = "osv" } -- debug any Lua code running in a Neovim instance
+  "mfussenegger/nvim-dap",
+  "theHamsta/nvim-dap-virtual-text",
+  "rcarriga/nvim-dap-ui",
+  "jay-babu/mason-nvim-dap.nvim",
+  { "jbyuki/one-small-step-for-vimkind", module = "osv" }, -- debug any Lua code running in a Neovim instance
   -- nvim-web-devicons
-  use "kyazdani42/nvim-web-devicons"
+  "kyazdani42/nvim-web-devicons",
   -- nvim-tree
-  use "kyazdani42/nvim-tree.lua"
+  "kyazdani42/nvim-tree.lua",
 
   -- gitsigns: git信息
-  use "lewis6991/gitsigns.nvim"
+  "lewis6991/gitsigns.nvim",
   -- feline:状态栏美化
-  use "feline-nvim/feline.nvim"
+  "feline-nvim/feline.nvim",
 
   -- nvim-autopairs:括号自动补全
-  use "windwp/nvim-autopairs"
+  "windwp/nvim-autopairs",
   -- nvim-surround: 自动前后插入括号
-  use "kylechui/nvim-surround"
+  "kylechui/nvim-surround",
   -- comment.nvim: 注释
-  use "numToStr/Comment.nvim"
+  "numToStr/Comment.nvim",
 
-  -- catppuccin.nvim:界面美化
-  -- 其它plugin
-  -- folke/tokyonight.nvim
-  -- EdenEast/nightfox.nvim
-  -- use {
-  --   "catppuccin/nvim",
-  --   -- 仓库命为nvim，有冲突，进行as做别名
-  --   as = "catppuccin",
-  --   config = function()
-  --     require("plugin-config.catppuccin")
-  --   end
-  -- }
-  use "folke/tokyonight.nvim"
+  "folke/tokyonight.nvim",
 
   -- 支持 LSP 状态的 buffer 栏
-  use {
+  {
     "akinsho/bufferline.nvim",
-    requires = "famiu/bufdelete.nvim", -- 删除 buffer 时不影响现有布局
-  }
+    dependencies = "famiu/bufdelete.nvim", -- 删除 buffer 时不影响现有布局
+  },
   -- telescope:模糊搜索
-  use {
+  {
     "nvim-telescope/telescope.nvim",
-    requires = {
+    dependencies = {
       -- required
       "nvim-lua/plenary.nvim", -- Lua开发模块
       -- options
@@ -169,12 +123,12 @@ return packer.startup(function (use)
       "nvim-telescope/telescope-ui-select.nvim", -- ui-select
       "nvim-telescope/telescope-dap.nvim", -- dap
     },
-  }
+  },
 
   -- 自动代码补全系列插件
-  use {
+  {
     "hrsh7th/nvim-cmp",  -- 代码补全核心插件，下面都是增强补全的体验插件
-    requires = {
+    dependencies = {
       {"onsails/lspkind-nvim"}, -- 为补全添加类似 vscode 的图标
       {"hrsh7th/vim-vsnip"}, -- vsnip 引擎，用于获得代码片段支持
       {"hrsh7th/cmp-vsnip"}, -- 适用于 vsnip 的代码片段源
@@ -187,68 +141,54 @@ return packer.startup(function (use)
       {"lukas-reineke/cmp-under-comparator"}, -- 让补全结果的排序更加智能
       -- {"tzachar/cmp-tabnine", run = "./install.sh"} -- tabnine 源,提供基于 AI 的智能补全
     }
-  }
+  },
 
   -- aerial.nvim: 代码大纲和快速导航
-  use "stevearc/aerial.nvim"
+  "stevearc/aerial.nvim",
 
   -- winshift: 重排窗口
-  use "sindrets/winshift.nvim"
+  "sindrets/winshift.nvim",
 
   -- indent-blankline: 显示缩进线
-  use "lukas-reineke/indent-blankline.nvim"
+  "lukas-reineke/indent-blankline.nvim",
 
   -- 显示网页色
-  use "norcalli/nvim-colorizer.lua"
+  "norcalli/nvim-colorizer.lua",
 
   -- nvim-treesitter: 语法高亮
-  use{
+  {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
-  }
-  use {
+  },
+  {
     "nvim-treesitter/nvim-treesitter-textobjects"
-  }
-  use "romgrk/nvim-treesitter-context" -- show class/function at the top
+  },
+  "romgrk/nvim-treesitter-context", -- show class/function at the top
 
   -- toggleterm: 终端
-  use "akinsho/toggleterm.nvim"
+  "akinsho/toggleterm.nvim",
 
   -- project: 工程管理
-  use "ahmedkhalf/project.nvim"
+  "ahmedkhalf/project.nvim",
 
   -- litee: 调用查询
-  use "ldelossa/litee.nvim"
-  use "ldelossa/litee-calltree.nvim"
+  "ldelossa/litee.nvim",
+  "ldelossa/litee-calltree.nvim",
   -- trouble: 错误显示
-  use "folke/trouble.nvim"
+  "folke/trouble.nvim",
 
   -- which key
-  use "folke/which-key.nvim"
+  "folke/which-key.nvim",
   -- alpha-nvim:启动界面
-  use "goolord/alpha-nvim"
+  "goolord/alpha-nvim",
   -- neoclip: neovim剪切板管理
-  use "AckslD/nvim-neoclip.lua"
-  use {
+  "AckslD/nvim-neoclip.lua",
+  {
     "phaazon/hop.nvim",
-    branch = 'v2', -- optional but strongly recommended
+    branch = "v2", -- optional but strongly recommended
     config = function()
       -- you can configure Hop the way you like here; see :h hop-config
-      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+      require"hop".setup { keys = "etovxqpdygfblzhckisuran" }
     end
-  }
-  --[[
-  -- windline
-  use {
-  "windwp/windline.nvim",
-  config = function()
-  require("plugin-config.windline")
-  end
-  }
-  --]]
-
-  if (PACKER_BOOTSTRAP) then
-    require("packer").sync()
-  end
-end
-)
+  },
+})
