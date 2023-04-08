3124c9369a872b1d0f75e146781c4de2c14b087d
CGinMax <cginmax1998@gmail.com>
2023-04-08 22:47:52 +0800

    fix format

---
+++ lua/plugins/conf/mason-null-ls.lua
@@ -0,0 +1,24 @@
+-- https://github.com/jay-babu/mason-null-ls.nvim
+local status_ok, mason_null_ls = pcall(require, 'mason-null-ls')
+if (not status_ok) then
+  require('utils').notifyError("jay-babu/mason-null-ls.nvim")
+  return
+end
+
+local status_ok, null_ls = pcall(require, "null-ls")
+if (not status_ok) then
+  require('utils').notifyError("jose-elias-alvarez/null-ls.nvim")
+  return
+end
+mason_null_ls.setup_handlers {
+    function(source_name, methods)
+      -- all sources with no handler get passed here
+
+      -- To keep the original functionality of `automatic_setup = true`,
+      -- please add the below.
+      require("mason-null-ls.automatic_setup")(source_name, methods)
+    end,
+    stylua = function(source_name, methods)
+      null_ls.register(null_ls.builtins.formatting.stylua)
+    end,
+}
